9e756bcd1efa984b0d8bf4d40c6925153c529572
Jarald Patterson <jeraldpatterson734@gmail.com>
2026-02-27 13:46:55 -0500

    Fix city name in LeadForm

--- src/components/LeadForm.tsx
+++ src/components/LeadForm.tsx
@@ -9,7 +9,7 @@ interface LeadFormProps {
 const LeadForm: React.FC<LeadFormProps> = ({ 
   title = "Request a Free Quote",
   phone = "(817) 632-4184",
-  city = "City"
+  city = "Hurst"
 }) => {
   const [formData, setFormData] = useState({
     name: '',
